169c567bc9cc23d622180cdf3fb5617711c9add2
Mauricio García <m4ur1c1o.gg@gmail.com>
2017-10-24 16:09:58 -0500

    Added new and create actions for faq item

--- app/controllers/faqs_controller.rb
+++ app/controllers/faqs_controller.rb
@@ -1,3 +1,25 @@
 class FaqsController < ApplicationController
-  belgons_to :project
+  def new
+    @project = Project.find(params[:project_id])
+    @faq = Faq.new
+  end
+
+  def create
+    @project = Project.find(params[:project_id])
+    @faq = Faq.new(faq_params.merge(project_id: @project.id))
+
+    respond_to do |format|
+      if @faq.save
+        format.html { redirect_to project_show_path(@project), notice: 'Your faq was created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+    def faq_params
+      params.require(:faq).permit(:question, :answer)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :faqs
   resources :projects, except: [:show] do
     member do
       get 'toggle_status'
     end
+    resources :faqs, except: [:index, :show]
   end
   get 'project/:id', to: 'projects#show', as: 'project_show'
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
