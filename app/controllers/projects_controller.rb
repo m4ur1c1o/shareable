d61f86cc3b74be6ba96d2b22f948bffe8ea43ec2
Mauricio García <m4ur1c1o.gg@gmail.com>
2017-10-24 13:52:01 -0500

    Integrated new and create actions for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,26 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params.merge(user_id: current_user.id))
+
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to projects_path, notice: 'Your project was created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+    def project_params
+      params.require(:project).permit(:title, :short_description, :description, :image)
+    end
 end
