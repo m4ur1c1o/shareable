066a0fe475c62c29e748806340e5ed5cb85233c7
Mauricio García <m4ur1c1o.gg@gmail.com>
2017-10-24 15:01:18 -0500

    Implemented concern for managing image data

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,15 @@
 class Project < ApplicationRecord
+  include Placeholder
   belongs_to :user
   enum status: { pending: 0, approved: 1 }
 
-  validates_presence_of :title, :short_description, :description
+  validates_presence_of :title, :short_description, :description, :image
+
+  after_initialize :set_defaults
+
+  private
+
+    def set_defaults
+      self.image ||= Placeholder.image_generator(height: '600', width: '400')
+    end
 end
