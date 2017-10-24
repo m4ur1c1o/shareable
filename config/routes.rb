9dfb7a21e59c1d7bc6541a4cdeb39a28962d28d0
Mauricio García <m4ur1c1o.gg@gmail.com>
2017-10-24 14:30:51 -0500

    Customized project routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :projects
+  resources :projects, except: [:show]
+  get 'project/:id', to: 'projects#show', as: 'project_show'
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   root to: 'pages#home'
 end
