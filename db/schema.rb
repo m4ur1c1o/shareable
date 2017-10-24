ee0bf979e6c30f79dae4b14bb9cd9977b34826a5
Mauricio García <m4ur1c1o.gg@gmail.com>
2017-10-24 12:26:39 -0500

    Added new parameters for user registration and editing account. Also changed role default value.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024165715) do
+ActiveRecord::Schema.define(version: 20171024172056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20171024165715) do
     t.inet "last_sign_in_ip"
     t.string "first_name"
     t.string "last_name"
-    t.integer "role"
+    t.integer "role", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
