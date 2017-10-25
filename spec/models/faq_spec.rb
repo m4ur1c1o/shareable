8bfb87f076c17bed873d3e53a242c5a08bc9416a
Mauricio García <m4ur1c1o.gg@gmail.com>
2017-10-24 22:59:58 -0500

    Added validations and test to faq

--- spec/models/faq_spec.rb
+++ spec/models/faq_spec.rb
@@ -1,4 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Faq, type: :model do
+  before do
+    @faq = FactoryGirl.build_stubbed(:faq)
+  end
+
+  describe 'creation' do
+    it 'can be created' do
+      expect(@faq).to be_valid
+    end
+  end
+
+  describe 'validations' do
+    it 'can not be created if not valid' do
+      @faq.question = nil
+      @faq.answer = nil
+      expect(@faq).to_not be_valid
+    end
+  end
 end
