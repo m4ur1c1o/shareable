6c51912a02faf2df1e4c3b8733607a0ec1af7e95
Mauricio García <m4ur1c1o.gg@gmail.com>
2017-10-24 20:04:11 -0500

    Implemented status span generator to show status properly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def status_label(status)
+    status_span_generator status
+  end
+
+  private
+
+    def status_span_generator(status)
+      case status
+      when 'pending'
+        content_tag(:span, status.titleize, class: 'label label-warning')
+      when 'approved'
+        content_tag(:span, status.titleize, class: 'label label-success')
+      end
+    end
 end
